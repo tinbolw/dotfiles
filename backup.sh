3be71fb410a1b1c5deb2d43d87dbd010f773d7ef
tinbolw <tinbolw@gmail.com>
2023-04-22 21:55:24 -0700

    initial commit

---
+++ backup.sh
@@ -0,0 +1,11 @@
+cp ~/.config/i3/config ./dotfiles/i3/config
+
+cp ~/.zshrc ./dotfiles/zsh/.zshrc
+
+cp ~/.config/polybar/config.ini ./dotfiles/polybar/config.ini
+cp ~/.config/polybar/launch.sh ./dotfiles/polybar/launch.sh
+
+cp ~/.config/rofi/config.rasi ./dotfiles/rofi/config.rasi
+cp ~/.config/rofi/theme.rasi ./dotfiles/rofi/theme.rasi
+
+cp ~/.config/alacritty/alacritty.yml ./dotfiles/alacritty/alacritty.yml
