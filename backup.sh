d2ce886814d3d2eff450125ce6a1a36a54b05516
tinbolw <tinbolw@gmail.com>
2023-07-19 13:33:57 -0700

    update

--- backup.sh
+++ backup.sh
@@ -1,14 +1,14 @@
-cp ~/.config/i3/config ./dotfiles/i3/config
+cp -v ~/.config/i3/config ./dotfiles/i3/config
 
-cp ~/.zshrc ./dotfiles/zsh/.zshrc
+cp -v ~/.zshrc ./dotfiles/zsh/.zshrc
 
-cp ~/.config/polybar/config.ini ./dotfiles/polybar/config.ini
-cp ~/.config/polybar/launch.sh ./dotfiles/polybar/launch.sh
+cp -v ~/.config/polybar/config.ini ./dotfiles/polybar/config.ini
+cp -v ~/.config/polybar/launch.sh ./dotfiles/polybar/launch.sh
 
-cp ~/.config/rofi/config.rasi ./dotfiles/rofi/config.rasi
-cp ~/.config/rofi/theme.rasi ./dotfiles/rofi/theme.rasi
+cp -v ~/.config/rofi/config.rasi ./dotfiles/rofi/config.rasi
+cp -v ~/.config/rofi/theme.rasi ./dotfiles/rofi/theme.rasi
 
-cp ~/.config/alacritty/alacritty.yml ./dotfiles/alacritty/alacritty.yml
+cp -v ~/.config/alacritty/alacritty.yml ./dotfiles/alacritty/alacritty.yml
 
 git commit -am "update"
 git push
