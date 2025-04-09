60df1d4f859a22d6b0d4456e0152ea97e690a460
Thomas Van <tinbolw@github.com>
2025-04-09 13:02:34 -0700

    updated backup script and i3 config, and added new configs

--- backup.sh
+++ backup.sh
@@ -1,14 +1,19 @@
-cp -v ~/.config/i3/config ./dotfiles/i3/config
+echo "Backing up ./dotfiles..."
 
-cp -v ~/.zshrc ./dotfiles/zsh/.zshrc
+cp -vr ./dotfiles ./dotfiles-backup
 
-cp -v ~/.config/polybar/config.ini ./dotfiles/polybar/config.ini
-cp -v ~/.config/polybar/launch.sh ./dotfiles/polybar/launch.sh
+echo "Starting backup..."
 
-cp -v ~/.config/rofi/config.rasi ./dotfiles/rofi/config.rasi
-cp -v ~/.config/rofi/theme.rasi ./dotfiles/rofi/theme.rasi
+# cp -v ~/.zshrc ./dotfiles/zsh/.zshrc
+# cp -v ~/.config/polybar/config.ini ./dotfiles/polybar/config.ini
+# cp -v ~/.config/polybar/launch.sh ./dotfiles/polybar/launch.sh
+# cp -vr ~/.config/alacritty/alacritty.yml ./dotfiles/alacritty/alacritty.yml
 
-cp -v ~/.config/alacritty/alacritty.yml ./dotfiles/alacritty/alacritty.yml
+cp -vr ~/.config/i3/config ./dotfiles/i3/config
+cp -vr ~/.config/i3status/config ./dotfiles/i3status/config
+cp -vr ~/.config/picom/picom.conf ./dotfiles/picom/picom.conf
 
-git commit -am "update"
-git push
+cp -vr ~/.config/ghostty/config ./dotfiles/ghostty/config
+
+cp -vr ~/.config/rofi/config.rasi ./dotfiles/rofi/config.rasi
+cp -vr ~/.config/rofi/theme.rasi ./dotfiles/rofi/theme.rasi
