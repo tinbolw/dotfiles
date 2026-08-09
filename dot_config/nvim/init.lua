9b5732072afc9f906f6a2d55d96854c9cbe413db
Thomas Van <tinbolw@gmail.com>
2026-08-08 21:01:47 -0700

    update dotfiles

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -81,6 +81,12 @@ require('nvim-treesitter').setup {
 
 -- LSP
 vim.lsp.enable('ts_ls')
+vim.lsp.config('qml-language-server', {
+    cmd = { 'qml-language-server' },
+    filetypes = { 'qml' },
+    root_markers = { { 'qmldir', 'shell.qml' }, '.git' },
+})
+vim.lsp.enable('qml-language-server')
 
 -- SYNTAX HIGHLIGHTING
 local languages = {
